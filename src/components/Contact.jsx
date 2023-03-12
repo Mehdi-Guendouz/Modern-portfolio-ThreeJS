46b25ef3f004a919f6ef36c0411140a676d7ada5
Guendouz mehdi <mahdign12@gmail.com>
2023-03-12 11:05:51 +0100

    email contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import styled from 'styled-components';
 import Maps from './Maps';
 
@@ -56,17 +57,37 @@ const Right = styled.div`
     flex: 1;
 `
 
+
 const Contact = () => {
+
+    const form = useRef();
+    const [Success, setSuccess] = useState(null);
+
+
+    const handelSubmit = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm('service_3m3h17h', 'template_elg6c9e', form.current, 'CQ0kvwObB2EV7aG6x')
+        .then((result) => {
+            console.log(result.text);
+            setSuccess(true)
+        }, (error) => {
+            console.log(error.text);
+            setSuccess(false)
+        });
+    }
+
     return (
         <Section>
             <Container>
                 <Left>
-                    <Form>
+                    <Form onSubmit={handelSubmit} ref={form}>
                         <Title>Contact Us</Title>
-                        <Input placeholder="Name" type="text" />
-                        <Input placeholder="email" type="email" />
-                        <TextArea placeholder="Write your message" rows={10}/>
-                        <Button>Send</Button>
+                        <Input placeholder="Name" type="text" name='name' />
+                        <Input placeholder="email" type="email" name='email' />
+                        <TextArea placeholder="Write your message" rows={10} name="message"/>
+                        <Button type='submit'>Send</Button>
+                        {Success && "Your message was sent successfully !!!!!!!!"}
                     </Form>
                 </Left>
                 <Right>
