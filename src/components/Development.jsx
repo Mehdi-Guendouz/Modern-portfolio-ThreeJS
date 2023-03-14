2fc82d3cd3fb52d7b009988e3e5132ee107eb3fb
Guendouz mehdi <mahdign12@gmail.com>
2023-03-14 21:50:45 +0100

    adding mac 3d Model

--- src/components/Development.jsx
+++ src/components/Development.jsx
@@ -1,10 +1,16 @@
+import { OrbitControls, Stage } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import React from 'react';
+import Mac from './Mac';
 
 const Development = () => {
     return (
-        <div>
-            development
-        </div>
+        <Canvas>
+            <Stage environment="city" intensity={0.6}  >
+                <Mac />
+            </Stage>
+            <OrbitControls enableZoom={false} />
+        </Canvas>
     );
 }
 
